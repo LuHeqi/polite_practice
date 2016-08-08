821a4c1144c490d08edd378ea38b932db26f1e27
polite <polite@gmial.com>
2016-08-08 13:59:56 +0800

    as normal test

---
+++ search-lucene/src/test/java/com/polite/other/TestSome.java
@@ -0,0 +1,14 @@
+package com.polite.other;
+
+/**
+ * @author polite
+ * @date 2016-08-08 .
+ */
+public class TestSome {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+    }
+
+
+}
+
